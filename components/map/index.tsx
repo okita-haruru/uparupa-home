d95244cb305ee34309430bca71a7dd37a1c42d1d
Okita_Haruru <okitaharuru@gmail.com>
2024-06-17 20:01:21 +0900

    change map to https

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -8,7 +8,7 @@ export const Map = () => {
     return (
         <div style={{width: '100%', height: '100vh', border: 'none'}}>
             <iframe
-                src="http://server.mygooooo.xyz:8124/"
+                src="https://server.mygooooo.xyz:8124/"
                 style={{width: '100%', height: '100%', border: 'none'}}
                 title="卫星地图"
             />
