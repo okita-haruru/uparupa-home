c9a6b72859c676bdf84787e5cab26f8aa59ad304
Okita_Haruru <okitaharuru@gmail.com>
2024-10-02 02:10:10 +0900

    update Cards

--- components/home/card-balance2.tsx
+++ components/home/card-balance2.tsx
@@ -1,18 +1,18 @@
 import { Card, CardBody } from "@nextui-org/react";
 import React from "react";
 // import { Community } from "../icons/community";
-
+import { FaQq } from "react-icons/fa";
 export const CardBalance2 = () => {
   return (
     <Card className="xl:max-w-sm bg-default-50 rounded-xl shadow-md px-3 w-full">
       <CardBody className="py-5">
-        {/*<div className="flex gap-2.5">*/}
-        {/*  <Community />*/}
-        {/*  <div className="flex flex-col">*/}
-        {/*    <span className="text-default-900">Health Insurance</span>*/}
-        {/*    <span className="text-default-900 text-xs">+2400 People</span>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
+        <div className="flex gap-2.5">
+          <FaQq size={25}/>
+          <div className="flex flex-col">
+            <span className="text-default-900">QQ群</span>
+            <span className="text-default-900 text-xs">634917584</span>
+          </div>
+        </div>
         {/*<div className="flex gap-2.5 py-2 items-center">*/}
         {/*  <span className="text-default-900 text-xl font-semibold">*/}
         {/*    $12,138*/}
--- components/home/card-balance3.tsx
+++ components/home/card-balance3.tsx
@@ -1,48 +1,23 @@
-import { Card, CardBody } from "@nextui-org/react";
+import {Card, CardBody} from "@nextui-org/react";
 import React from "react";
 // import { Community } from "../icons/community";
+import {Link} from "@nextui-org/link";
+import {FaDropbox} from "react-icons/fa";
 
 export const CardBalance3 = () => {
-  return (
-    <Card className="xl:max-w-sm bg-success rounded-xl shadow-md px-3 w-full">
-      <CardBody className="py-5">
-        {/*<div className="flex gap-2.5">*/}
-        {/*  <Community />*/}
-        {/*  <div className="flex flex-col">*/}
-        {/*    <span className="text-white">Card Insurance</span>*/}
-        {/*    <span className="text-white text-xs">1311 Cars</span>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-        {/*<div className="flex gap-2.5 py-2 items-center">*/}
-        {/*  <span className="text-white text-xl font-semibold">$3,910</span>*/}
-        {/*  <span className="text-danger text-xs">- 4.5%</span>*/}
-        {/*</div>*/}
-        {/*<div className="flex items-center gap-6">*/}
-        {/*  <div>*/}
-        {/*    <div>*/}
-        {/*      <span className="font-semibold text-danger text-xs">{"↓"}</span>*/}
-        {/*      <span className="text-xs">100,930</span>*/}
-        {/*    </div>*/}
-        {/*    <span className="text-white text-xs">USD</span>*/}
-        {/*  </div>*/}
-
-        {/*  <div>*/}
-        {/*    <div>*/}
-        {/*      <span className="font-semibold text-danger text-xs">{"↑"}</span>*/}
-        {/*      <span className="text-xs">4,120</span>*/}
-        {/*    </div>*/}
-        {/*    <span className="text-white text-xs">USD</span>*/}
-        {/*  </div>*/}
-
-        {/*  <div>*/}
-        {/*    <div>*/}
-        {/*      <span className="font-semibold text-danger text-xs">{"⭐"}</span>*/}
-        {/*      <span className="text-xs">125</span>*/}
-        {/*    </div>*/}
-        {/*    <span className="text-white text-xs">VIP</span>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </CardBody>
-    </Card>
-  );
+    return (
+        <Link href="/update-log">
+            <Card className="xl:max-w-sm bg-primary rounded-xl shadow-md px-3 w-full">
+                <CardBody className="py-5">
+                    <div className="flex gap-2.5">
+                        <FaDropbox size={25}/>
+                        <div className="flex flex-col">
+                            <span className="text-white">当前版本:ver.1.0</span>
+                            <span className="text-white text-xs">点击查看更新日志</span>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
+        </Link>
+    );
 };
