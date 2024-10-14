bf03521300ab6a993bb0eeef4625cdd090e91825
ColaTea <keriteal985@gmail.com>
2024-10-14 17:31:07 +0800

    backup code

--- app/api/developers/route.ts
+++ app/api/developers/route.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-require('dotenv').config();
+import {NextResponse} from "next/server";
 
 const GITHUB_API_URL = 'https://api.github.com';
 const TOKEN = process.env.GITHUB_TOKEN;
@@ -8,29 +7,125 @@ const ORGANIZATION = process.env.ORGANIZATION;
 const TEAM = process.env.GITHUB_TEAM;
 
 const headers = {
-  'Authorization': `token ${TOKEN}`,
-  'Accept': 'application/vnd.github.v3+json'
-}
+  'Authorization': `Bearer ${TOKEN}`,
+  'Accept': 'application/vnd.github+json'
+};
 
 interface GithubUser {
   login: string;
+  url: string;
   avatar: string;
   html_url: string;
+}
+
+interface GithubRepo {
+  name: string;
+  full_name: string;
+  url: string;
+}
+
+interface GithubContributor {
+  total: number;
+  weeks: { w: number, a: number, d: number, c: number }[];
+  author: {
+    login: string;
+  }
 }
 
 export interface DeveloperInfo {
   username: string;
   displayName: string;
   user_page: string;
+  contributions: DeveloperContribute;
+}
+
+export interface Contribution {
+  commits: number;
+  additions: number;
+  deletions: number;
+}
+
+export interface DeveloperContribute {
+  commits: number;
+  additions: number;
+  deletions: number;
+  usernam
+}
+
+export interface RepoContributor {
+  repo: string;
+  contributors: DeveloperContribute[];
+}
+
+const getName = async (user: GithubUser | undefined) => {
+  if (!user) return '';
+  const resp = await axios.get(user.url, {headers});
+  return resp.data.name || user.login;
 }
 
 export async function GET() {
   try {
-    const members: GithubUser[] = await axios.get(`${GITHUB_API_URL}/orgs/${ORGANIZATION}/teams/${TEAM}/members`, {headers})
-      .then(resp => resp.data);
+    const [members, repositories] = await Promise.all([
+      axios.get<GithubUser[]>(`${GITHUB_API_URL}/orgs/${ORGANIZATION}/teams/${TEAM}/members`, {headers}).then(resp => resp.data),
+      axios.get<GithubRepo[]>(`${GITHUB_API_URL}/orgs/${ORGANIZATION}/repos`, {headers}).then(resp => resp.data)
+    ]);
 
-    console.log(members);
+    const memberUsernames = new Set(members.map(member => member.login));
+
+    const summary = await Promise.all(members.map(async (member) => {
+      return {
+        username: member.login,
+        displayName: await getName(member),
+        user_page: member.html_url,
+        contributions: {
+          commits: 0,
+          additions: 0,
+          deletions: 0
+        }
+      } as DeveloperInfo;
+    }))
+
+    const details: RepoContributor[] = await Promise.all(repositories.map(async ({url, full_name}) => {
+      const repoContributors: GithubContributor[] = await axios.get(`${url}/stats/contributors`, {headers})
+        .then(resp => {
+          const data = resp.data as GithubContributor[];
+          if (!Array.isArray(data)) {
+            console.error(`Unexpected stats format for ${full_name}, ${resp.status}:`, data);
+            return [];
+          }
+          return data;
+        })
+        .catch(error => {
+          console.log(url, 'Contributors Error:', error);
+          return [];
+        });
+
+      return {
+        repo: full_name,
+        contributors: await Promise.all(repoContributors
+          .filter(contributor => memberUsernames.has(contributor.author.login))
+          .map(async (contributor): Promise<DeveloperContribute> => ({
+            commits: contributor.total,
+            additions: contributor.weeks.reduce((acc, week) => acc + week.a, 0),
+            deletions: contributor.weeks.reduce((acc, week) => acc + week.d, 0),
+          })))
+      };
+    }));
+
+    details.forEach(detail => {
+      detail.contributors.forEach(contributor => {
+        const member = summary.find(member => member.username === contributor);
+        if (member) {
+          member.contributions.commits += contributor.commits;
+          member.contributions.additions += contributor.additions;
+          member.contributions.deletions += contributor.deletions;
+        }
+      });
+    })
+
+    return NextResponse.json({summary, details});
   } catch (error) {
     console.error(error);
+    return NextResponse.json({error});
   }
 }
