34fe178c7c563d3fc630378ae2057c0bdc6ba2b0
Okita_Haruru <okitaharuru@gmail.com>
2024-11-28 01:03:34 +0900

    update API_URL

--- config/apiconfig.ts
+++ config/apiconfig.ts
@@ -3,5 +3,5 @@ dotenv.config();
 
 export const { SERVER_API_BASE } = process.env;
 
-export const API_URL = 'https://server.mygooooo.xyz:9090';
+export const API_URL = 'https://server.uparupa.town:9090';
 // export const API_URL = 'http://127.0.0.1:8080';
