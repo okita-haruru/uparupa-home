86c37e6ad97a169503e59d921ddd1a1b785a5cac
ColaTea <keriteal985@gmail.com>
2024-11-30 02:16:54 +0800

    fix: Trying fix 500 when searching

--- app/api/searching/route.ts
+++ app/api/searching/route.ts
@@ -24,34 +24,42 @@ export interface SearchResult {
 }
 
 export async function GET(request: NextRequest): Promise<NextResponse<ApiResponse<SearchResult[]>>> {
-    const keyword = request.nextUrl.searchParams.get("keyword") || "";
-    const data = await axios.get(`${SERVER_API_BASE}/players`).then(resp => resp.data.data as PlayerData[]);
-    const filteredData = data.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase())) || [];
-    console.log("Searching", keyword, "Players", filteredData);
+    try {
+        const keyword = request.nextUrl.searchParams.get("keyword") || "";
+        const data = await axios.get(`${SERVER_API_BASE}/players`).then(resp => resp.data.data as PlayerData[]);
+        const filteredData = data.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase())) || [];
+        console.log("Searching", keyword, "Players", filteredData);
 
-    const encodedKeyword = encodeURIComponent(keyword);
-    console.log("Url", `https://zh.minecraft.wiki/rest.php/v1/search/title?q=${encodedKeyword}&limit=10`);
-    const wikiSuggestion = await axios.get(`https://zh.minecraft.wiki/rest.php/v1/search/title?q=${encodedKeyword}&limit=10`)
-        .then(resp => resp.data.pages as WikiSuggestion[]) || [];
-    console.log("Wiki", encodedKeyword, "Suggestion", wikiSuggestion);
-    return NextResponse.json({
-        success: true,
-        message: "获取玩家信息成功",
-        data: [
-            ...filteredData.map(item => {
-                return {
-                    text: item.name,
-                    icon: item.avatar,
-                    type: "player" as const
-                }
-            }),
-            ...wikiSuggestion.map(item => {
-                return {
-                    text: item.title,
-                    icon: item.thumbnail?.url || "",
-                    type: "wiki" as const
-                }
-            })
-        ]
-    });
+        const encodedKeyword = encodeURIComponent(keyword);
+        console.log("Url", `https://zh.minecraft.wiki/rest.php/v1/search/title?q=${encodedKeyword}&limit=10`);
+        const wikiSuggestion = await axios.get(`https://zh.minecraft.wiki/rest.php/v1/search/title?q=${encodedKeyword}&limit=10`)
+            .then(resp => resp.data.pages as WikiSuggestion[]) || [];
+        console.log("Wiki", encodedKeyword, "Suggestion", wikiSuggestion);
+        return NextResponse.json({
+            success: true,
+            message: "获取玩家信息成功",
+            data: [
+                ...filteredData.map(item => {
+                    return {
+                        text: item.name,
+                        icon: item.avatar,
+                        type: "player" as const
+                    }
+                }),
+                ...wikiSuggestion.map(item => {
+                    return {
+                        text: item.title,
+                        icon: item.thumbnail?.url || "",
+                        type: "wiki" as const
+                    }
+                })
+            ]
+        });
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: `获取玩家信息失败: ${error}`,
+            data: []
+        });
+    }
 }
